32537caebf1c9083c0724ab70bae57085162ba0a
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2020-12-30 07:30:49 -0400

    Diseno de la pagina de busqueda de peliculas capitulo 153 , seccion 10

--- seccion10peliculas/peliculasApp/src/app/tab2/tab2.page.ts
+++ seccion10peliculas/peliculasApp/src/app/tab2/tab2.page.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   textoBuscar: string= '';
+  ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos', 'La vida es bella'];
 
   buscar(event){
-
-console.log(event);
-
+     //console.log(event);
+    const valor = event.detail.value;
   }
 
   constructor() {}
