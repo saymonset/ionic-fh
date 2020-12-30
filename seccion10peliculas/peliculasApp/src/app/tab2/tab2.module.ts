dbeba49bc57cec6ae4dff8e8a957f0100f03cf7a
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2020-12-30 07:07:32 -0400

    fixs tabs mal ubicados

--- seccion10peliculas/peliculasApp/src/app/tab2/tab2.module.ts
+++ seccion10peliculas/peliculasApp/src/app/tab2/tab2.module.ts
@@ -19,3 +19,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   declarations: [Tab2Page]
 })
 export class Tab2PageModule {}
+
+
+
+ 
--- seccion10peliculas/peliculasApp/src/app/tab2/tab2.page.ts
+++ seccion10peliculas/peliculasApp/src/app/tab2/tab2.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  textoBuscar: string= '';
+
+  buscar(event){
+
+console.log(event);
+
+  }
+
   constructor() {}
 
 }
